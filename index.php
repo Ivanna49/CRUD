5052e6948bb7465011ebeda7d19807c24af67178
Ivanna López Barbona <109434696+Ivanna49@users.noreply.github.com>
2024-11-25 13:46:24 -0300

    First Commit
    Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pantalla de Inicio</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="logo"> <img src="img/300px-Evento_Pokémon_singulares_del_20_aniversario.png"
+                alt="Logo del Proyecto">
+            <h1>Mi CRUD Pokémon</h1>
+        </div>
+        <div class="options"> <button class="btn"
+                onclick="window.location.href='Auth/register.php'">Registrarse</button> <button class="btn"
+                onclick="window.location.href='Auth/login.php'">Iniciar Sesión</button> </div>
+    </div>
+
+</body>
+
+</html>
